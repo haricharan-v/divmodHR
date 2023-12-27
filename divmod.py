3f8ec058e37f3fdacb10693e27072b13e981f8f6
haricharan-v <66981624+haricharan-v@users.noreply.github.com>
2023-12-27 10:21:11 -0500

    Update divmod.py

--- divmod.py
+++ divmod.py
@@ -4,8 +4,8 @@ def dividedfunc(para_a, para_b):
     divmod_output = divmod(para_a, para_b)
 
     print(f"\nThe divided result is: {div_output}")
-    print(f"The divided result is: {mod_output}")
-    print(f"The divided result is: {divmod_output}")
+    print(f"The remainder of the two inputs is: {mod_output}")
+    print(f"The divmod result is: {divmod_output}")
 
 
 input_a = int(input("Enter the first number: "))
